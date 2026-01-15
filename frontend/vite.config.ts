436f2fee803b4643dad4a31cbe1509d7cf2cb9d2
AzielJr <aziel@sistemap.com.br>
2026-01-14 21:36:20 -0300

    Force new build hash to invalidate Vercel cache

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   build: {
     sourcemap: 'hidden',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
   },
   plugins: [
     react({
