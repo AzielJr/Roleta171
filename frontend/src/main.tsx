326fcfa38d760cb93819bdf0a36a57ed61aaa7c8
AzielJr <aziel@sistemap.com.br>
2026-01-18 00:14:39 -0300

    chore: Force rebuild para limpar cache do Vercel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Build: 2026-01-18-00:14 - Force cache clear
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
